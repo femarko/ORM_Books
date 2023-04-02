f74a2ab47e83e5f0e0c7fb77c848965866e38da8
Марк <fenster.mark@yandex.ru>
2023-04-03 00:45:20 +0300

    in the pr

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import sqlalchemy as sq
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 # from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
@@ -7,14 +7,25 @@ class Publisher(Base):
     __tablename__ = 'publisher'
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=48), unique=True)
-    def __str__(self):
-        return f'{self.id} : {self.name}'
 
 class Book(Base):
     __tablename__ = 'book'
     id = sq.Column(sq.Integer, primary_key=True)
     title = sq.Column(sq.String(length=48), unique=True)
-    id_publisher = jhj
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
+    publisher = relationship(Publisher, backref='books')
+
+class Shop(Base):
+    __tablename__ = 'shop'
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=48), unique=True)
+
+class Stock(Base):
+    __tablename__ = 'stock'
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
+    count = sq.Column(sq.Integer, nullable=True)
 
 def create_tables(engine):
     Base.metadata.drop_all(engine)
