e3dbb46c23f5466fbedcce1b4e9c1f48f096fa72
Марк <fenster.mark@yandex.ru>
2023-04-06 18:09:29 +0300

    looks like it works...

--- Main.py
+++ Main.py
@@ -22,12 +22,17 @@ session = Session()
 #     print(res)
 
 # Объединение справа-налево
-subq = session.query(Shop.name, Stock.id).select_from(Shop).join(Stock).join(Sale).subquery()
-main_query = session.query(Book.title, Shop.name).select_from(Sale).\
-    join(Stock).join(Book).join(Publisher).join(subq, Sale.id_stock == Stock.id)
+# subq = session.query(Shop.name, Stock.id).select_from(Shop).join(Stock).join(Sale).subquery()
+# main_query = session.query(Book.title, Shop.name).select_from(Sale).\
+#     join(Stock).join(Book).join(Publisher).join(subq, Sale.id_stock == Stock.id)
+#
+# for res in main_query:
+#     print(res)
+#
+# print(subq)
+# print(main_query)
 
-for res in main_query:
-    print(res)
-
-print(subq)
-print(main_query)
+query_result = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).select_from(Publisher)\
+    .join(Book).join(Stock).join(Sale).join(Shop).filter(Publisher.name == 'Пушкин')
+for res in query_result:
+    print(f'{res[0]} {res[1]} {res[2]} {res[3]}')
