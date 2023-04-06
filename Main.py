88993a8acbeb881320536d41f6c2b6a71a88dc94
Марк <fenster.mark@yandex.ru>
2023-04-06 18:36:47 +0300

    Final commit

--- Main.py
+++ Main.py
@@ -2,37 +2,17 @@ import sqlalchemy as sq
 from sqlalchemy.orm import sessionmaker
 import config
 import psycopg2
-import json
 from models import create_tables, Publisher, Book, Sale, Shop, Stock
 
-# requested_publisher = input('Введите автора / издательство: ')
-
 DSN = config.dsn_str
-
 engine = sq.create_engine(DSN)
-# create_tables(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Объединение слева- направо
-# shop_sample = session.query(Shop.name, Stock.id_book).join(Stock.shop).join(Sale).subquery()
-#     # print(c)
-# for res in session.query(Book.title, Publisher.name, Shop.name).join(Publisher).join(shop_sample, Book.id == shop_sample.c.id_book):
-#     print(res)
-
-# Объединение справа-налево
-# subq = session.query(Shop.name, Stock.id).select_from(Shop).join(Stock).join(Sale).subquery()
-# main_query = session.query(Book.title, Shop.name).select_from(Sale).\
-#     join(Stock).join(Book).join(Publisher).join(subq, Sale.id_stock == Stock.id)
-#
-# for res in main_query:
-#     print(res)
-#
-# print(subq)
-# print(main_query)
-
+requested_publisher = input('Введите автора / издательство: ')
 query_result = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).select_from(Publisher)\
-    .join(Book).join(Stock).join(Sale).join(Shop).filter(Publisher.name == 'Пушкин')
+    .join(Book).join(Stock).join(Sale).join(Shop).filter(Publisher.name == requested_publisher)
+
 for res in query_result:
-    print(f'{res[0]} {res[1]} {res[2]} {res[3]}')
+    print(f'{res[0]}\t{res[1]}\t{res[2]}\t{res[3]}')
